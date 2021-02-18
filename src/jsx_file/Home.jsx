f4c4c33bb92b468d81279343d4c863551bf45c8f
Yasharth dubey <yashdubeywinner@gmail.com>
2021-02-18 22:42:24 +0530

    login page

--- src/jsx_file/Home.jsx
+++ src/jsx_file/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import web from "../images/4180157-removebg-preview.png";
 import web1 from "../images/student-removebg.png"
 import web2 from "../images/company.jpg";
 import web3 from "../images/admin.jpg";
 import web4 from "../images/Capture-removebg-preview.png";
+import { Button, Modal, Form, Input, Radio } from 'antd';
+import "antd/dist/antd.css";
 import "../index.css";
 import"../css/home.css";
 const Home =() => {
@@ -47,7 +49,7 @@ const Home =() => {
                                 <img src={web1} className = "student card_us" alt = "home img"/>
                                 <h5 class="card-title"><strong>For Student</strong></h5>
                                 <p class="card-text"></p>
-                                <a href="#" class="btn ">Login</a>
+                                <Buttons name="Student Login"/>
                             </div>
                         </div>
                     </div>
@@ -57,7 +59,7 @@ const Home =() => {
                             <img src={web2} className = "company card_us" alt = "home img"/>
                                 <h5 class="card-title"><strong>For Company</strong></h5>
                                 <p class="card-text"></p>
-                                <a href="#" class="btn ">Login</a>
+                                <Buttons name="Company Login"/>
                             </div>
                         </div>
                     </div>
@@ -67,8 +69,8 @@ const Home =() => {
                             <img src={web3} className = "company card_us" alt = "home img"/>
                                 <h5 class="card-title card_us"><strong>For Admin</strong></h5>
                                 <p class="card-text"></p>
-                                <a href="#" class="btn">Login</a>
-                            </div>
+                                <Buttons name="Admin Login"/></div>
+                            
                         </div>
                     </div>
                 </div>
@@ -78,5 +80,95 @@ const Home =() => {
         </>
     );
 };
+class Buttons extends React.Component {
+  render(){
+     // popup and form code
+      const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
+          const [form] = Form.useForm();
+          return (
+            <Modal
+              visible={visible}
+              title={this.props.name}
+              okText="Login"
+              cancelText="Cancel"
+              onCancel={onCancel}
+              onOk={() => {
+                form
+                  .validateFields()
+                  .then((values) => {
+                    form.resetFields();
+                    onCreate(values);
+                  })
+                  .catch((info) => {
+                    console.log('Validate Failed:', info);
+                  });
+              }}
+            >
+              <Form 
+                form={form}
+                layout="vertical"
+                name="form_in_modal"
+                initialValues={{
+                  modifier: 'public',
+                }}
+              >
+                <Form.Item
+                  name="username"
+                  label="User Name"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please enter username!',
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item name="password" label="Password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please enter password!',
+                  },
+                ]}
+                >
+                  <Input type="password" />
+                </Form.Item>
+                
+              </Form>
+            </Modal>
+          );
+        };
+        
+        const CollectionsPage = () => {
+          const [visible, setVisible] = useState(false);
+        
+          const onCreate = (values) => {
+            console.log('Received values of form: ', values);
+            setVisible(false);
+          };
+        
+          return (
+            <div>
+                <a className="btn" onClick={() => { setVisible(true);}}>
+                Login
+              </a>
+              <CollectionCreateForm
+                visible={visible}
+                onCreate={onCreate}
+                onCancel={() => {
+                  setVisible(false);
+                }}
+              />
+            </div>
+          );
+        };
+    return (
+        <div>
+            <CollectionsPage/>
+       </div>
+    );
+  }
+  }
 
 export default Home;
