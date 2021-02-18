1e073d56a35da618d9b18254443ad125f29c4431
Yasharth dubey <yashdubeywinner@gmail.com>
2021-02-18 23:20:35 +0530

    login

--- src/jsx_file/Home.jsx
+++ src/jsx_file/Home.jsx
@@ -4,6 +4,7 @@ import web1 from "../images/student-removebg.png"
 import web2 from "../images/company.jpg";
 import web3 from "../images/admin.jpg";
 import web4 from "../images/Capture-removebg-preview.png";
+import web5 from "../images/login.jpg"
 import { Button, Modal, Form, Input, Radio } from 'antd';
 import "antd/dist/antd.css";
 import "../index.css";
@@ -87,59 +88,60 @@ class Buttons extends React.Component {
           const [form] = Form.useForm();
           return (
             <Modal
-              visible={visible}
-              title={this.props.name}
-              okText="Login"
-              cancelText="Cancel"
-              onCancel={onCancel}
-              onOk={() => {
-                form
-                  .validateFields()
-                  .then((values) => {
-                    form.resetFields();
-                    onCreate(values);
-                  })
-                  .catch((info) => {
-                    console.log('Validate Failed:', info);
-                  });
-              }}
-            >
-              <Form 
-                form={form}
-                layout="vertical"
-                name="form_in_modal"
-                initialValues={{
-                  modifier: 'public',
-                }}
-              >
-                <Form.Item
-                  name="username"
-                  label="User Name"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'Please enter username!',
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item name="password" label="Password"
+            visible={visible}
+            title={this.props.name}
+            okText="Login"
+            cancelText="Cancel"
+            onCancel={onCancel}
+            onOk={() => {
+              form
+                .validateFields()
+                .then((values) => {
+                  form.resetFields();
+                  onCreate(values);
+                })
+                .catch((info) => {
+                  console.log('Validate Failed:', info);
+                });
+            }}
+          >
+              <section className="card_us">
+              <img src={web5} className = "login_img card_us" alt = "home img"/>
+              </section>              
+            <Form 
+            
+              form={form}
+              layout="vertical"
+              name="form_in_modal"
+              initialValues={{
+                modifier: 'public',
+              }}>
+              <Form.Item 
+                name="username"
+                label="User Name"
                 rules={[
                   {
                     required: true,
-                    message: 'Please enter password!',
+                    message: 'Please enter username!',
                   },
                 ]}
-                >
-                  <Input type="password" />
-                </Form.Item>
-                
-              </Form>
-            </Modal>
+              >
+                <Input className="login_form "/>
+              </Form.Item>
+              <Form.Item name="password" label="Password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter password!',
+                },
+              ]}
+              >
+                <Input type="password" className="login_form"/>
+              </Form.Item>  
+            </Form>
+          </Modal>
           );
         };
-        
         const CollectionsPage = () => {
           const [visible, setVisible] = useState(false);
         
@@ -147,7 +149,6 @@ class Buttons extends React.Component {
             console.log('Received values of form: ', values);
             setVisible(false);
           };
-        
           return (
             <div>
                 <a className="btn" onClick={() => { setVisible(true);}}>
