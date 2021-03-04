4333efc7b328c70844dd1c45ee664fe9a371c64e
Shresth Bharadia <57027463+shresth2102001@users.noreply.github.com>
2021-03-04 19:05:57 +0530

    admin home

--- src/jsx_file/adminhome.jsx
+++ src/jsx_file/adminhome.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
-import web from "../images/about.png";
 import "../index.css";
 import"../css/home.css";
 import "../css/login.css";
-import Navbar_admin from './navbar_admin';
-import {Form, Input} from 'antd';
-const Companyhome =() => {
+import Navbar_Admin from './navbar_admin';
+import ReactPlayer from 'react-player'
+function Cards(props) {
+    return (
+                <>
+                   <div className="card2 magin-top" >
+                        <div className="card-body profile_width pop">
+                          <div><img src={props.image} className="profile_img2 marginl"></img></div>
+                          <div className="cb"><div className="cb2"><strong><h4 className="card-title  centers fsize2">{props.name}</h4></strong>
+                          <p className="card-text centers"><strong>Location : </strong> {props.location}</p></div>
+                            <div className="details">
+                            <p className="card-text  fsize" ><strong>Email:</strong> {props.email}</p>
+                            <p className="card-text  fsize"><strong> Year of Establishment:</strong> {props.college}</p>
+                            <p className="card-text  fsize"><strong>Discription :</strong>  {props.discription}</p>
+                            <ReactPlayer url={props.url} className='vediosize'/>
+                            <div className='pop' >
+                            <a href="#" className="btn btn-primary text_size left_m">Edit</a></div>
+                            </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    </>
+    )
+                    }
+const Adminhome =() => {
     return (
         <>
-        <Navbar_admin/>
-        <section className="">
-    
-          
+        <Navbar_Admin />
+        <section className="pop image width"> 
+        <div>
+            <Cards name="IIIT Dharwad" image="https://upload.wikimedia.org/wikipedia/en/c/c1/Indian_Institute_of_Information_Technology%2C_Dharwad.png"/>
+        </div> 
         </section>
         </>
     );
 };
 
-export default Companyhome;
+export default Adminhome;
+
