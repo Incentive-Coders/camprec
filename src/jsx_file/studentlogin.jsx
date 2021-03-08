bdd3f6f665efb0f708ee796750dc9e0183884976
Shresth Bharadia <57027463+shresth2102001@users.noreply.github.com>
2021-03-08 21:03:04 +0530

    student login api link

--- src/jsx_file/studentlogin.jsx
+++ src/jsx_file/studentlogin.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React} from 'react';
 import "E:/camprec2/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../index.css";
 import"../css/home.css";
@@ -8,6 +8,37 @@ import Navbar from "./Navbar";
 import FormItem from 'antd/lib/form/FormItem';
 import {Form, Input} from 'antd';
 import { NavLink } from 'react-router-dom';
+import { render } from '@testing-library/react';
+function login_link (){
+    // POST request using fetch with error handling
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Accept':'*/*','Content-Type': 'application/json' },
+        body: JSON.stringify(
+                                { 
+                                email: document.getElementById('email'),
+                                password: document.getElementById('password')  
+                                }
+                            )
+    };
+    fetch('https://camprec.herokuapp.com/api/student/login', requestOptions)
+        .then(async response => {
+            const data = await response.json();
+
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response status
+                const error = (data && data.message) || response.status;
+                return Promise.reject(error);
+            }
+            render(
+                <NavLink to="/studenthome"/>
+            )    
+        })
+        .catch(error => {
+            console.error('There was an error!');
+        });
+}
 const Studentlogin =() => {
     return (
         <>
@@ -21,17 +52,15 @@ const Studentlogin =() => {
                                 <Form >
                                     <FormItem>
                                     <div className="form-group margin-t">
-                                        <Input type="text" className="form-control" placeholder="Username" required/>
+                                        <Input type="text" id= 'email' className="form-control" placeholder="Username" required/>
                                     </div>
                                     </FormItem>
                                     <FormItem>
                                     <div className="form-group margin-t">
-                                        <Input type="password" className="form-control" placeholder="password" required/>
+                                        <Input type="password" id= 'password' className="form-control" placeholder="password" required/>
                                     </div>
                                     </FormItem>
-                                    <NavLink to='/studenthome'>
-                                    <Input type= "submit" className="btn margin-t" value="Login"/>
-                                    </NavLink>
+                                    <Input type= "submit" className="btn margin-t" value="Login" onClick={login_link()}/>
                                 </Form>
                     </div>
                 </div>
