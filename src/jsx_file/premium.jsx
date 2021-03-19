64a2c7f4708b873ae28f5365cd577dc641155d95
Yasharth dubey <yashdubeywinner@gmail.com>
2021-03-19 20:00:48 +0530

    added payment method

---
+++ src/jsx_file/premium.jsx
@@ -0,0 +1,96 @@
+import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
+import React from "react"
+import PaymentForm from "./PaymentForm"
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import axios from "axios"
+import React, { useState } from 'react'
+import "../css/premium.css"
+
+const PUBLIC_KEY = "pk_test_rgWMA3zxjAtwaB6iV8b5W40x"
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY)
+
+export default function StripeContainer() {
+	return (
+		<Elements stripe={stripeTestPromise}>
+			<PaymentForm />
+		</Elements>
+	)
+}
+
+const CARD_OPTIONS = {
+	iconStyle: "solid",
+	style: {
+		base: {
+			iconColor: "#c4f0ff",
+			color: "#fff",
+			fontWeight: 500,
+			fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+			fontSize: "16px",
+			fontSmoothing: "antialiased",
+			":-webkit-autofill": { color: "#fce883" },
+			"::placeholder": { color: "#87bbfd" }
+		},
+		invalid: {
+			iconColor: "#ffc7ee",
+			color: "#ffc7ee"
+		}
+	}
+}
+
+export default function PaymentForm() {
+    const [success, setSuccess ] = useState(false)
+    const stripe = useStripe()
+    const elements = useElements()
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: "card",
+            card: elements.getElement(CardElement)
+        })
+
+
+    if(!error) {
+        try {
+            const {id} = paymentMethod
+            const response = await axios.post("https://camprec.herokuapp.com/api/payment", {
+                amount: 1000,
+                id
+            })
+
+            if(response.data.success) {
+                console.log("Successful payment")
+                setSuccess(true)
+            }
+
+        } catch (error) {
+            console.log("Error", error)
+        }
+    } else {
+        console.log(error.message)
+    }
+}
+
+    return (
+        <>
+        {!success ? 
+        <form onSubmit={handleSubmit}>
+            <fieldset className="FormGroup">
+                <div className="FormRow">
+                    <CardElement options={CARD_OPTIONS}/>
+                </div>
+            </fieldset>
+            <button>Pay</button>
+        </form>
+        :
+       <div>
+           <h2>You just bought a sweet spatula congrats this is the best decision of you're life</h2>
+       </div> 
+        }
+            
+        </>
+    )
+}
