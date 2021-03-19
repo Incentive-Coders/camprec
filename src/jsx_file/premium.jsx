04ecc9945701d62b0cfb37efd96a9906a3b91c5c
Yasharth dubey <yashdubeywinner@gmail.com>
2021-03-19 20:27:39 +0530

    changed payment

--- src/jsx_file/premium.jsx
+++ src/jsx_file/premium.jsx
@@ -1,96 +1,76 @@
-import { Elements } from "@stripe/react-stripe-js"
-import { loadStripe } from "@stripe/stripe-js"
-import React from "react"
-import PaymentForm from "./PaymentForm"
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
-import axios from "axios"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
 import "../css/premium.css"
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe("pk_test_51IVziGIyZFM8agXoThfXEqwer9bkatYn6mvviooDbpoZcpA0lau5WnGD7TuiYstO6gfwUQHyIDqoN0pMIsVbjsyY001YH2hQH5");
 
-const PUBLIC_KEY = "pk_test_rgWMA3zxjAtwaB6iV8b5W40x"
+const ProductDisplay = ({ handleClick }) => (
+  <section className = "lols">
+    <div className="product">
+      <img className = "imj"
+        src="https://i.imgur.com/EHyR2nP.png"
+        alt="The cover of Stubborn Attachments"
+      />
+      <div className="description">
+        <h3>Stubborn Attachments</h3>
+        <h5>$20.00</h5>
+      </div>
+    </div>
+    <button type="button" id="checkout-button" role="link" onClick={handleClick}>
+      Checkout
+    </button>
+  </section>
+);
 
-const stripeTestPromise = loadStripe(PUBLIC_KEY)
+const Message = ({ message }) => (
+  <section className = "lols">
+    <p className = "imp">{message}</p>
+  </section>
+);
 
-export default function StripeContainer() {
-	return (
-		<Elements stripe={stripeTestPromise}>
-			<PaymentForm />
-		</Elements>
-	)
-}
+export default function App() {
+  const [message, setMessage] = useState("");
 
-const CARD_OPTIONS = {
-	iconStyle: "solid",
-	style: {
-		base: {
-			iconColor: "#c4f0ff",
-			color: "#fff",
-			fontWeight: 500,
-			fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-			fontSize: "16px",
-			fontSmoothing: "antialiased",
-			":-webkit-autofill": { color: "#fce883" },
-			"::placeholder": { color: "#87bbfd" }
-		},
-		invalid: {
-			iconColor: "#ffc7ee",
-			color: "#ffc7ee"
-		}
-	}
-}
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
 
-export default function PaymentForm() {
-    const [success, setSuccess ] = useState(false)
-    const stripe = useStripe()
-    const elements = useElements()
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const {error, paymentMethod} = await stripe.createPaymentMethod({
-            type: "card",
-            card: elements.getElement(CardElement)
-        })
-
-
-    if(!error) {
-        try {
-            const {id} = paymentMethod
-            const response = await axios.post("https://camprec.herokuapp.com/api/payment", {
-                amount: 1000,
-                id
-            })
-
-            if(response.data.success) {
-                console.log("Successful payment")
-                setSuccess(true)
-            }
-
-        } catch (error) {
-            console.log("Error", error)
-        }
-    } else {
-        console.log(error.message)
+    if (query.get("success")) {
+      setMessage("Order placed! You will receive an email confirmation.");
     }
-}
 
-    return (
-        <>
-        {!success ? 
-        <form onSubmit={handleSubmit}>
-            <fieldset className="FormGroup">
-                <div className="FormRow">
-                    <CardElement options={CARD_OPTIONS}/>
-                </div>
-            </fieldset>
-            <button>Pay</button>
-        </form>
-        :
-       <div>
-           <h2>You just bought a sweet spatula congrats this is the best decision of you're life</h2>
-       </div> 
-        }
-            
-        </>
-    )
+    if (query.get("canceled")) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
+  const handleClick = async (event) => {
+    const stripe = await stripePromise;
+
+    const response = await fetch("https://camprec.herokuapp.com/api/checkout", {
+      method: "POST",
+    });
+
+    const session = await response.json();
+
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+    }
+  };
+
+  return message ? (
+    <Message message={message} />
+  ) : (
+    <ProductDisplay handleClick={handleClick} />
+  );
 }
