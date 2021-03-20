a6d3792b3a812a9ec9c3642de50901abb79575b4
Shresth Bharadia <57027463+shresth2102001@users.noreply.github.com>
2021-03-20 14:45:14 +0530

    1

--- src/jsx_file/companyjobs.jsx
+++ src/jsx_file/companyjobs.jsx
@@ -12,6 +12,7 @@ function Cards(props) {
                    <div className="card widths" >
                         <div className="card-body">
                             <strong><h4 className="card-title  centers">{props.title}</h4></strong>
+                            <strong><h6 className="card-title  centers">{props.location}</h6></strong>
                             <p className="card-text">{props.content}</p>
                             <div className='pop' >
                             <a href="#" className="btn btn-primary text_size margin_left">Show Aplicants</a>
@@ -24,18 +25,18 @@ function Cards(props) {
     )
                     }
 const Cardlist = ({ Joblist }) => {
-    console.log(Joblist);
     return (
         <div className="gridwraper">
         {
             Joblist.map((user, i) => {
-            return (
+            return (<>
                 <Cards
                 key={i}
                 title={Joblist[i].job_title}
                 content={Joblist[i].job_description}
                 location = {Joblist[i].location}
                 />
+                </>
             );
             })
         }
@@ -46,22 +47,18 @@ class Companyjobs extends Component {
 
     constructor(props){
     super(props);
-    
-    this.state = {
-        company_id : '',
-    }
+    this.body =JSON.stringify({ 
+        "company_id" : '6036269f37ec673ed0415b17', 
+    });
+    this.header = {'Accept':'*/*','Content-Type': 'application/json', 'Accept-Encoding' : 'gzip, deflate, br', 'Connection' : 'keep-alive'};
     this.companyjoblist = this.companyjoblist.bind(this);
     }
 
     companyjoblist (){
         var job = [];
         // POST request using fetch with error handling
-        const body =  
-            { 
-                "company_id" : '6036269f37ec673ed0415b17', 
-            };
-        const header = {'Accept':'*/*','Content-Type': 'application/json', 'Accept-Encoding' : 'gzip, deflate, br', 'Connection' : 'keep-alive'};
-        axios.post('https://camprec.herokuapp.com/api/jobs/list',body,{header})
+        
+        axios.post('https://camprec.herokuapp.com/api/jobs/list',this.body,this.header)
             .then(function(response) {
                 // check for error response
                 if (response.status != 200) {
@@ -83,6 +80,7 @@ class Companyjobs extends Component {
 
 
     render() {
+        console.log(this.companyjoblist())
         return (
             <>
             <Navbar_company />
