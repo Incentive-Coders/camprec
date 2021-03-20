ed976f0fe64a34ca58e93833dd42afb07d012704
Shresth Bharadia <57027463+shresth2102001@users.noreply.github.com>
2021-03-20 13:46:02 +0530

    jio

--- src/jsx_file/companyjobs.jsx
+++ src/jsx_file/companyjobs.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import {React, Component} from 'react';
 import "../index.css";
 import"../css/home.css";
 import "../css/login.css";
 import { NavLink } from "react-router-dom";
 import Navbar_company from './navbar_company';
 import web from "../images/add.png";
+import axios from 'axios';
 function Cards(props) {
     return (
                 <>
@@ -22,31 +23,86 @@ function Cards(props) {
                     </>
     )
                     }
-const Companyjobs =() => {
+const Cardlist = ({ Joblist }) => {
     return (
-        <>
-        <Navbar_company />
-        <section className="image width">
-                    <div className = "get_started">
-                            <h2 className='text_center margintop'>
-                             <strong className = "brand-name leftmargin">Jobs</strong>
-                            </h2>
-                    </div>
-                    <div className="gridwraper">
-                        <Cards title="Jobs" content="Some quick example text to build on the job title and make up the bulk of the job's content."/>
-                        <Cards title="Jobs" content="Some quick example text to build on the job title and make up the bulk of the job's content."/>
-                        <Cards title="Jobs" content="Some quick example text to build on the job title and make up the bulk of the job's content."/>
-                        <Cards title="Jobs" content="Some quick example text to build on the job title and make up the bulk of the job's content."/>
-                        <Cards title="Jobs" content="Some quick example text to build on the job title and make up the bulk of the job's content."/>
-                        <div className="card3 widths" >
-                        <div className="card-body">
-                            <NavLink to='/addjobs'><img src={web} className="addheight"/></NavLink> 
-                        </div>
-                    </div>
-                    </div>
-        </section>
-        </>
+        <div className="gridwraper">
+        {
+            Joblist.map((user, i) => {
+            return (
+                <Cards
+                key={i}
+                title={Joblist[i].job_title}
+                content={Joblist[i].job_description}
+                />
+            );
+            })
+        }
+        </div>
     );
-};
+    }
+class Companyjobs extends Component {
+
+    constructor(props){
+    super(props);
+    
+    this.state = {
+        company_id : '',
+    }
+    this.companyjoblist = this.companyjoblist.bind(this);
+    }
+
+    companyjoblist (){
+        console.log(this.state.company_id);
+        // POST request using fetch with error handling
+        const body =  
+            { 
+                "company_id" : '6036269f37ec673ed0415b17', 
+            };
+        const header = {'Accept':'*/*','Content-Type': 'application/json', 'Accept-Encoding' : 'gzip, deflate, br', 'Connection' : 'keep-alive'};
+        axios.post('https://camprec.herokuapp.com/api/jobs/list',body,{header})
+            .then(function(response) {
+                const data = response.data;
+                // check for error response
+                if (response.status != 200) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+                console.log(response.data)
+                return response.data;
+        })
+         .catch(error => {
+                console.error('There was an error!');
+                window.alert("somethin went wrong")
+                
+            });
+            
+    }; 
+
+
+    render() {
+        var Joblist=this.companyjoblist
+        return (
+            <>
+            <Navbar_company />
+            <section className="image width heights">
+                        <div className = "get_started">
+                                <h2 className='text_center margintop'>
+                                <strong className = "brand-name leftmargin">Jobs</strong>
+                                </h2>
+                        </div>
+                        <div className="gridwraper">
+                            <Cardlist Joblist={Joblist}/>
+                            <div className="card3 widths" >
+                            <div className="card-body">
+                                <NavLink to='/addjobs'><img src={web} className="addheight"/></NavLink> 
+                            </div>
+                        </div>
+                        </div>
+            </section>
+            </>
+        );
+    };
+}
 
 export default Companyjobs ;
